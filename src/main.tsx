af380615cf71214682e284c3226de8edd61ff6cd
Makajeez <buhari.alhassan0@gmail.com>
2026-06-20 20:50:46 +0100

    config: notistack

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import { SnackbarProvider } from "notistack";
+
 import './App.css'
 import App from './app/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <SnackbarProvider maxSnack={3} anchorOrigin={{ vertical: "top", horizontal: "center" }}>
+      <App />
+    </SnackbarProvider>
   </StrictMode>,
 )
